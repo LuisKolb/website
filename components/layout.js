cb3b23e3bfa75f9b6c6567fee098c1392b761b91
Luis Kolb <e1622731@student.tuwien.ac.at>
2021-04-24 07:48:12 +0200

    styling, also a bit o' the ol' content

--- components/layout.js
+++ components/layout.js
@@ -3,38 +3,54 @@ import Image from "next/image";
 import Link from "next/link";
 
 const name = "Luis Kolb";
+const greetings = ["Hey there!", "What's up?", "Hello there!", "( ^_^)／", "(≧▽≦)", "👋"];
 export const siteTitle = "Luis Kolb | Hey there!";
 
 export default function Layout({ children, home }) {
     return (
-        <div className="grid grid-cols-4 w-screen bg-gray-500">
-            <Head>
-                <link rel="icon" href="/favicon.ico" />
-                <meta name="description" content="Luis Kolb" />
-                <meta property="og:image" content={`https://og-image.vercel.app/${encodeURI(siteTitle)}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`} />
-                <meta name="og:title" content={siteTitle} />
-                <meta name="twitter:card" content="summary_large_image" />
-            </Head>
-            <header className="col-span-1 h-screen sticky flex flex-col items-start mx-auto pt-2 space-y-2">
-                <Image priority src="/images/bossman.jpg" className="rounded-full" height={144} width={144} alt={name} />
-                <h1 className="text-center text-white text-xl w-full">{name}</h1>
-                <section className="flex flex-row content-center justify-between w-full">
-                    <a href="https://github.com/LuisKolb/" className="">
-                        <img src="/github.svg" className="h-12 w-12"></img>
-                    </a>
-                    <a href="https://www.linkedin.com/in/luis-kolb/" className="">
-                        <img src="/linkedin.svg" className="h-12 w-12"></img>
-                    </a>
-                </section>
-                {home && (
-                    <div className="absolute bottom-2">
-                        <Link href="/">
-                            <a>← Back to home</a>
-                        </Link>
+        <div className="bg-gray-700">
+            <div className="max-w-7xl min-w-min ml-auto mr-auto grid grid-cols-4 w-full">
+                <Head>
+                    <link rel="icon" href="/favicon.ico" />
+                    <meta name="description" content="Luis Kolb" />
+                    <meta property="og:image" content={`https://og-image.vercel.app/${encodeURI(siteTitle)}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`} />
+                    <meta name="og:title" content={siteTitle} />
+                    <meta name="twitter:card" content="summary_large_image" />
+                </Head>
+
+                <header className="flex flex-col col-span-4 lg:col-span-1 lg:h-screen sticky items-start mx-auto p-8 lg:p-16 space-y-8 bg-gray-700">
+
+                    <div className="flex flex-row items-center lg:flex-col lg:space-y-4 space-x-4 lg:space-x-0">
+                        <Image priority src="/images/bossman.jpg" className="rounded-full" height={144} width={144} alt={name} />
+                        <div className="text-center text-white text-md w-full">
+                            <div className="text-3xl mb-2 text-red-400">Luis Kolb</div>
+                            {greetings[Math.floor(Math.random() * greetings.length)]}
+                        </div>
                     </div>
-                )}
-            </header>
-            <main className="container col-span-3 flex flex-col space-y-2 text-yellow-100">{children}</main>
+
+                    <section className="flex flex-row content-center justify-between w-full">
+                        <a href="https://github.com/LuisKolb/" className="">
+                            <img src="/github.svg" className="h-8 w-8"></img>
+                        </a>
+                        <a href="https://www.linkedin.com/in/luis-kolb/" className="">
+                            <img src="/linkedin.svg" className="h-8 w-8"></img>
+                        </a>
+                        <a href="mailto:kolb.luis@gmail.com" className="">
+                            <img src="/email.svg" className="h-8 w-8"></img>
+                        </a>
+                    </section>
+
+                    {!home && (
+                        <div className="absolute bottom-8 text-blue-400">
+                            <Link href="/">
+                                <a>← Back to home</a>
+                            </Link>
+                        </div>
+                    )}
+
+                </header>
+                <main className="lg:max-w-2xl col-span-4 lg:col-span-3 flex flex-col p-8 lg:mr-24 text-white bg-gray-500">{children}</main>
+            </div>
         </div>
     );
 }
--- pages/index.js
+++ pages/index.js
@@ -7,13 +7,64 @@ export default function Home() {
             <Head>
                 <title>{siteTitle}</title>
             </Head>
-            <section className="mt-2">
-                Currently working as...
-                <br />
+            {/* Current Projects */}
+            <section className="text-lg pb-4 space-y-2">
+                <div className="flex flex-row">
+                    <div className="text-2xl">Current Projects</div>
+                    <div className="bg-red-400 rounded-full flex-grow h-1 my-auto ml-6"></div>
+                </div>
+                <div className="ml-4">
+                    &#62; 
+                    <img src="/vuejs.svg" className="inline h-5 w-5 ml-2" alt="vuejs"></img> + 🔥 + 🌬
+                    <a href="https://social-app-ddf21.web.app/" className="mx-2 text-blue-300">
+                        Webapp with live chat, kanban and <div className="inline italic">The Oracle </div>
+                    </a>
+                </div>
+                <div className="ml-4">
+                    &#62; 1st Level Support @
+                    <a href="https://www.austriatech.at/" className="mx-2 text-blue-300">
+                        Austriatech
+                    </a>
+                </div>
+                <div className="ml-4">&#62; Studying Medical Computer Science @ TU Vienna</div>
+                <div className="ml-4">
+                    &#62; Interested in Data Science? Check out my
+                    <a href="#" className="ml-2 text-blue-300">
+                        Bachelor's Thesis
+                    </a>
+                    !
+                </div>
             </section>
-            <section className="">
-                CV
-                <br />
+
+            {/* CV Section */}
+            <section className="text-lg pt-4 space-y-2">
+                <div className="flex flex-row">
+                    <div className="text-2xl">Ye Tales of Olde</div>
+                    <div className="bg-red-400 rounded-full flex-grow h-1 my-auto ml-6"></div>
+                </div>
+
+                <div className="ml-4">&#62; Bsc. Medical Computer Science from TU Vienna</div>
+                <div className="ml-4">&#62; Civil Service @ Austrian Red Cross - IT Department</div>
+                <div className="ml-4">&#62; High School Diploma from Wiedner Gymnasium GRG4</div>
+                <div className="ml-4">
+                    &#62; 
+                    <a href="#" className="ml-2 text-red-300">
+                        Download this as a PDF
+                    </a>
+                </div>
+            </section>
+
+            {/* Other Random Stuff */}
+            <section className="text-lg pt-4 space-y-2">
+                <div className="flex flex-row">
+                    <div className="text-2xl">Blog Posts</div>
+                    <div className="bg-red-400 rounded-full flex-grow h-1 my-auto ml-6"></div>
+                </div>
+
+                <div className="ml-4">&#62; (╯°□°)╯︵ ┻━┻</div>
+                <div className="ml-4">&#62; ...</div>
+                <div className="ml-4">&#62; ┬─┬ノ( º _ ºノ)</div>
+                <div className="ml-4">&#62; 😥👉👈</div>                    
             </section>
         </Layout>
     );
